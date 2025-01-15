83d6868b0c6200ac3eff9f592ce751e5c9e1eb1d
DurlaVerder <vrrrr227@gmail.com>
2025-01-15 18:35:05 +0300

    fix

--- initDataBase.sql
+++ initDataBase.sql
@@ -2,8 +2,8 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     login VARCHAR(255) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,  -- Хэшированный пароль
-    user_name VARCHAR(255),
+    password VARCHAR(255) NOT NULL,  -- Хэшированный пароль
+    username VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
